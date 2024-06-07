b90bbcf39bd7812f1e62f0f13e512318fa398b94
Herbert Lange <github@pietaetskirsche.de>
2024-06-07 17:53:36 +0200

    make exporter more flexible automatically adding columns for token level annotations

--- sbx_retreat2024_plugin/sbx_table_export/table_export.py
+++ sbx_retreat2024_plugin/sbx_table_export/table_export.py
@@ -1,7 +1,10 @@
 """Example for a custom exporter."""
 
-from sparv.api import Annotation, Export, Config, exporter
+from sparv.api import Annotation, Export, ExportAnnotations, Config, exporter, get_logger
 from typing import Optional
+
+
+logger = get_logger(__name__)
 
 @exporter("Export annotations in a tabular format", config = [
     Config("sbx_retreat2024_plugin.table_export.lemma",
@@ -9,26 +12,25 @@ from typing import Optional
         )
 def table_exporter(
     words: Annotation = Annotation("<token:word>"),
-    lemmas: Optional[Annotation] = Annotation("[sbx_retreat2024_plugin.table_export.lemma]"),
-        # Annotation("<token>:stanza.baseform"),
+    annotations : ExportAnnotations =  ExportAnnotations("export.annotations"),
     sentence: Annotation = Annotation("<sentence>"),
     out : Export = Export("sbx_retreat2024_plugin.table_export/{file}.txt")):
     lines = []
     sent_id = 1
     sentences, _orphans = sentence.get_children(words)  # Ignore the orphans 😢
     words_list = list(words.read())
-    has_lemmas = False
-    if lemmas is not None:
-        has_lemmas = True
-        lemmas_list = list(lemmas.read())
+    annotation_lists={a.name: list(a.read()) for (a,_) in annotations}
     for s in sentences:
-        lines.append("# sentence " + str(sent_id))
+        line = ["# sentence", str(sent_id)]
+        line.extend([annotation_name + ":" + annotation_lists[annotation_name][sent_id-1] for annotation_name in annotation_lists if annotation_name.startswith("segment.sentence:")])
+        lines.append(" ".join(line))
+        lines.append("segment.token:word\t" + "\t".join([annotation_name for annotation_name in annotation_lists if annotation_name.startswith("segment.token:")]))
         sent_id += 1
         for t in s:
-            line = words_list[t]
-            if has_lemmas:
-                line += "\t" + lemmas_list[t]
-            lines.append(line)
+            line = []
+            line.append(words_list[t])
+            line.extend([annotation_lists[annotation_name][t] for annotation_name in annotation_lists if annotation_name.startswith("segment.token:")])
+            lines.append("\t".join(line))
     # Write result to file
     with open(out, "w", encoding="utf-8") as f:
         f.write("\n".join(lines))
